fda2312692b32255909decc23ac36e6883bf540b
Adam Laycock <adam@alaycock.co.uk>
2024-07-06 17:43:15 +0100

    allow the id to be passed to header image function

--- fns/header_or_featured_image.php
+++ fns/header_or_featured_image.php
@@ -1,7 +1,11 @@
 <?php
-function wordpress_fns_header_or_featured_image($imageSize){
-  if(get_the_post_thumbnail(null, $size = $imageSize)){
-    $attachmentImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id(), $imageSize);
+function wordpress_fns_header_or_featured_image($imageSize, $postId = 0){
+  if($postId === 0){
+    $postId = get_the_id();
+  }
+
+  if(get_the_post_thumbnail($postId, $size = $imageSize)){
+    $attachmentImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id($postId), $imageSize);
     return $attachmentImageSrc[0];
   }
   
